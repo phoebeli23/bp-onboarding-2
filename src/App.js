212e8a5d673698a407db8b878414bbe7545a5946
Phoebe Li <liphoebe01@gmail.com>
2020-10-03 22:37:23 -0700

    finish app no styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Airtable from 'airtable';
+import Goal from './components/Goal';
+
+const base = new Airtable({apiKey: 'keySvaJa6xF65khTO'}).base('app4wIksRPqk47dPw');
 
 function App() {
+
+  const [goals, setGoals] = useState([]);
+  const [updates, setUpdates] = useState([]);
+
+  useEffect(() => {
+    base('goals')
+      .select({ view: "Grid view" })
+      .eachPage((records, fetchNextPage) => {
+        setGoals(records);
+        fetchNextPage();
+      });
+    base('updates')
+      .select({ view: "Grid view" })
+      .eachPage((records, fetchNextPage) => {
+        setUpdates(records);
+        fetchNextPage();
+      });
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>My Goals</h1>
+    {
+      goals.map(goal => (
+        <Goal
+          key={goal.id}
+          goal={goal}
+          updates={updates.filter(update => update.fields.goalid[0] === goal.id)}
+        />
+      ))
+    }
+    </>
   );
 }
 
